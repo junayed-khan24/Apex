e14c038a52bf0969d2aee498017e95aa7544f52b
freelancerjunayed junayed <freelancerjunayedjunayed@gmail.com>
2026-04-28 23:14:51 +0600

    first commit

---
+++ src/router/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h2 className="text-6xl">Home</h2>
+    }
+])
+
+
+export default router;
